77b5b3421d192716d9c13a5a742b77f00d2eac26
pairing4good <pairing4good@gmail.com>
2022-11-03 08:20:29 -0500

    updated tests and readme

--- README.md
+++ README.md
@@ -1,12 +1,23 @@
-# 021 Combinators
+# 022 Pseudo Classes
 
-[![021 Combinators](https://img.youtube.com/vi/SXeC5wSuKOE/0.jpg)](https://www.youtube.com/watch?v=SXeC5wSuKOE)
+[![022 Pseudo Classes](https://img.youtube.com/vi/L-Xc-SdaNjs/0.jpg)](https://www.youtube.com/watch?v=L-Xc-SdaNjs)
 
 The `index.html` file
-- should style all descendant divs within span should should have orange text
-- should style all children divs of span should should have large text
-- should style all adjacent sibling divs of span should should have red text
-- should style all sibling divs of span should should a yellow background
+- should add styling so that hovering over the anchor will turn the anchor text orange
+- should add styling so that initially the paragraph tooltip is not visible
+- should add styling so that hovering over the "Hover over me." text will display the paragraph tooltip
 
 Resources:
-- https://www.w3schools.com/css/css_combinators.asp
+- https://www.w3schools.com/css/css_pseudo_classes.asp
+
+
+Add styling so that:
+
+hovering over the anchor will turn the anchor text purple
+
+initially the paragraph tooltip is not visible
+
+hovering over the "Hover over me." text will display the paragraph tooltip
+
+Resources:
+
--- test/verify.test.js
+++ test/verify.test.js
@@ -37,74 +37,33 @@ afterEach(async () => {
   await browser.close();
 });
 
-describe('all descendant divs within span', () => {
-  it('should should have orange text', async () => {
-    const colors = await page.$$eval('span div', (divs) => {
-      let colors = [];
-      for(let i = 0; i < divs.length; i++){
-        let style = window.getComputedStyle(divs[i]);
-        colors.push(style.getPropertyValue('color'));
-      }
-      return colors;
+describe('hovering over the anchor', () => {
+  it('should turn the anchor text purple', async () => {
+    const matches = await page.$eval('style', (style) => {
+      return style.innerHTML.match(/a:hover.*{[\s\S][^}]*color.*:.*purple.*;/g).length;
     });
-
-    expect(colors.length).toBe(3)
     
-    expect(colors[0]).toBe('rgb(255, 165, 0)');
-    expect(colors[1]).toBe('rgb(255, 165, 0)');
-    expect(colors[2]).toBe('rgb(255, 165, 0)');
+    expect(matches).toEqual(1);
   });
 });
 
-describe('all children divs of span', () => {
-  it('should should have large text', async () => {
-    const fontSizes = await page.$$eval('span > div', (divs) => {
-      let fontSizes = [];
-      for(let i = 0; i < divs.length; i++){
-        let style = window.getComputedStyle(divs[i]);
-        fontSizes.push(style.getPropertyValue('font-size'));
-      }
-      return fontSizes;
+describe('initially the paragraph tooltip', () => {
+  it('should not be visible', async () => {
+    const display = await page.$eval('div p', (paragraph) => {
+      let style = window.getComputedStyle(paragraph);
+      return style.getPropertyValue('display');
     });
       
-    expect(fontSizes.length).toBe(2);
-      
-    expect(fontSizes[0]).toBe('18px');
-    expect(fontSizes[1]).toBe('18px');
+    expect(display).toBe('none');
   });
 });
 
-describe('all adjacent sibling divs of span', () => {
-  it('should should have red text', async () => {
-    const colors = await page.$$eval('span + div', (divs) => {
-      let colors = [];
-      for(let i = 0; i < divs.length; i++){
-        let style = window.getComputedStyle(divs[i]);
-        colors.push(style.getPropertyValue('color'));
-      }
-      return colors
+describe('hovering over the "Hover over me." text', () => {
+  it('should display the paragraph tooltip', async () => {
+    const matches = await page.$eval('style', (style) => {
+      return style.innerHTML.match(/div:hover.*p {[\s\S][^}]*display.*:.*inline.*;/g).length;
     });
-
-    expect(colors.length).toBe(1);
-      
-    expect(colors[0]).toBe('rgb(255, 0, 0)');
-  });
-});
-
-describe('all sibling divs of span', () => {
-  it('should should a yellow background', async () => {
-    const backgroundColors = await page.$$eval('span ~ div', (divs) => {
-      let backgroundColors = [];
-      for(let i = 0; i < divs.length; i++){
-        let style = window.getComputedStyle(divs[i]);
-        backgroundColors.push(style.getPropertyValue('background-color'));
-      }
-      return backgroundColors;
-    });
-      
-    expect(backgroundColors.length).toBe(2)
-
-    expect(backgroundColors[0]).toBe('rgb(255, 255, 0)');
-    expect(backgroundColors[1]).toBe('rgb(255, 255, 0)');
+    
+    expect(matches).toEqual(1);
   });
 });
